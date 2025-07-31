6629917d21b85c08c162d1cc47ae632ced1f7406
Ojas Mhatre <mhatreojas192@gmail.com>
2025-07-31 12:54:35 +0530

    changes

--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -6,7 +6,12 @@ const Header = () => {
       <h1 className="text-3xl md:text-4xl lg:text-5xl font light m-0">
         CodeIQ
       </h1>
-    
+      <nav className='hidden md:flex items-center gap-12'>
+        <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href=""> Company</a>
+        <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href=""> Company</a>
+        <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href=""> Company</a>
+        <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href=""> Company</a>
+      </nav>
    </header>
   )
 }
