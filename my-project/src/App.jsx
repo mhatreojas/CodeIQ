50907e8d95a4f8235a96b7bcee20ded29b841e00
Ojas Mhatre <mhatreojas192@gmail.com>
2025-07-31 12:43:35 +0530

    Changes in Header

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,4 +1,4 @@
-
+import Header from "./components/Header"
 
 
 export default function App() {
@@ -7,6 +7,7 @@ export default function App() {
        { }
        <img  className="absolute top-0 right-0 opacity-60 -z-1"src="/gradient.png" alt="Gradient-img"/>
        <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"></div>
+       <Header/>
     </main>
   )
 }
---
+++ my-project/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+   <header className="flex justify-between items-center py-4 px-4 lg:px-20">
+      <h1 className="text-3xl md:text-4xl lg:text-5xl font light m-0">
+        CodeIQ
+      </h1>
+    
+   </header>
+  )
+}
+
+export default Header
