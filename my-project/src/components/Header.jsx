3baf8f412b3bcd7c299320067ba3842dc45e219e
Ojas Mhatre <mhatreojas192@gmail.com>
2025-07-31 18:37:24 +0530

    added animation

--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -18,10 +18,18 @@ const Header = () => {
         CodeIQ
       </h1>
       <nav className='hidden md:flex items-center gap-12'>
-        <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href=""> Company</a>
-        <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href=""> Company</a>
-        <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href=""> Company</a>
-        <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href=""> Company</a>
+        <a  data-aos="fade-down"
+     data-aos-easing="linear"
+     data-aos-duration="1000" className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href=""> Company</a>
+        <a data-aos="fade-down"
+     data-aos-easing="linear"
+     data-aos-duration="1500" className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href=""> Company</a>
+        <a data-aos="fade-down"
+     data-aos-easing="linear"
+     data-aos-duration="2000" className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href=""> Company</a>
+        <a data-aos="fade-down"
+     data-aos-easing="linear"
+     data-aos-duration="2500" className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href=""> Company</a>
         
       </nav>
       <button className='hidden md:block bg-[#a7a7a7] text-black py-3 px-8 rounded-full border-none font-medium transition-all duration-500 hover:bg-white cursor-pointer z-50'>
--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -4,10 +4,12 @@ import Spline from '@splinetool/react-spline'
 const Hero = () => {
   return (
     <main className='flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h-[calc(90vh-6rem)]'>
-  <div className='max-w-xl ml-[5%] z-10 mt-[90%] md:mt-[60%] lg:mt-0'>
+  <div data-aos="fade-right"
+     data-aos-offset="300"
+     data-aos-easing="ease-in-sine" className='max-w-xl ml-[5%] z-10 mt-[90%] md:mt-[60%] lg:mt-0'>
     
     
-    <div className=' font-serif relative w-[95%] sm:w-48 h-10 bg-gradient-to-r from-[#656565] to-[#e99b63] shadow-[0_0_15px_rgba(255,255,255,0.4)] rounded-full'>
+    <div  className=' font-serif relative w-[95%] sm:w-48 h-10 bg-gradient-to-r from-[#656565] to-[#e99b63] shadow-[0_0_15px_rgba(255,255,255,0.4)] rounded-full'>
       <div className='absolute inset-[3px] bg-black rounded-full flex items-center justify-center gap-1'>
         <i class='bx bx-diamond'> INTRODUCTION</i>
       </div>
@@ -29,8 +31,11 @@ const Hero = () => {
      <a href="" className='border border-[#2a2a2a] py-2 sm:py-3 px-4 sm:px-5 rounded-full sm:text-lg text-sm font-semibold tracking-wider transition-all duration-300 hover:bg-[#1a1a1a] bg-gray-300 text-black hover:text-white'>Get started</a>
   </div>
     </div>
-      <Spline className='absolute lg:top-0 top-[-20%] bottom-0 lg-left[25%] sm:left-[-2%] h-full' scene="https://prod.spline.design/2PwOBnlUzeLoGKbv/scene.splinecode" />
-
+      <Spline data-aos="fade-zoom-in"
+     data-aos-easing="ease-in-back"
+     data-aos-delay="300"
+     data-aos-offset="0" data-aos-duration="3000" className='absolute lg:top-0 top-[-20%] bottom-0 lg:left-[25%] sm:left-[-2%] h-full' scene="https://prod.spline.design/2PwOBnlUzeLoGKbv/scene.splinecode" />
+     
     </main>
   )
 }
