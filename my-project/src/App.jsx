318f3af05ad6db80b1932718794b3e8c9162f929
Ojas Mhatre <mhatreojas192@gmail.com>
2025-07-31 16:05:27 +0530

    added hero section

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header"
-
+import Hero from "./components/Hero"
 
 export default function App() {
   return (
@@ -8,6 +8,7 @@ export default function App() {
        <img  className="absolute top-0 right-0 opacity-60 -z-1"src="/gradient.png" alt="Gradient-img"/>
        <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"></div>
        <Header/>
+       <Hero/>
     </main>
   )
 }
---
+++ my-project/src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import 'boxicons/css/boxicons.min.css' 
+
+const Hero = () => {
+  return (
+    <main className='flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h-[calc(90vh-6rem)]'>
+  <div className='max-w-xl ml-[5%] z-10 mt-[90%] md:mt-[60%] lg:mt-0'>
+    
+    
+    <div>
+      <div>
+
+      </div>
+    </div>
+    </div>
+  
+    </main>
+  )
+}
+
+export default Hero
